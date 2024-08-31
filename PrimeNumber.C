9c84f0c796001b39fb3ab4b5be230069927872fe
Megha2005-meg <meghaeangadiangadi@gmail.com>
2024-08-31 12:57:29 +0530

    Create PrimeNumber.C

---
+++ PrimeNumber.C
@@ -0,0 +1,24 @@
+#include<stdio.h> 
+void main() 
+{
+int m,n,d;
+printf("Enter the number\n"); 
+scanf("%d",&n); 
+m=n; 
+int rev=0;
+while(n!=0)
+{
+ d=n%10;
+ rev=rev*10+d;
+ n=n/10;
+}
+printf("%d\n",rev);
+if(rev==m) 
+{
+ printf("the number ispalidrome\n"); 
+}
+ else 
+{
+printf("number is not palidrome\n"); 
+}
+}
