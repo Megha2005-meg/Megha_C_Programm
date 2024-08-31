b9a4de8961db37168c55f0937860ca10f6eda83e
Megha2005-meg <meghaeangadiangadi@gmail.com>
2024-08-31 13:17:51 +0530

    Create SumOfTwoMatrix.C

---
+++ SumOfTwoMatrix.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main()
+{
+int b[10][10],a[10][10],i,j,m,n,k,l;
+printf("\nenter the a array size\n");
+scanf("%d%d",&k,&l);
+for(i=1;i<=k;i++)
+{
+ for(j=1;j<=l;j++)
+ {
+  printf("enter the elements");
+  scanf("%d",&a[i][j]);
+ }
+}
+printf("\nenter the b array size\n");
+scanf("%d%d",&n,&m);
+for(i=1;i<=n;i++) 
+{
+ for(j=1;j<=m;j++) 
+ {
+  printf("enter the elements\n");
+  scanf("%d",&b[i][j]); 
+ }
+}
+printf("the sum of two matrixare\n");
+for(i=1;i<=n;i++) 
+{
+ for(j=1;j<=m;j++) 
+ {
+  printf(" %da",b[i][j]+a[i][j]); 
+ }
+ printf("\n"); 
+ }
+}
